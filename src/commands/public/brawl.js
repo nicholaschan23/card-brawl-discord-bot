8aefda0b1e5d66c8643c9a36de2eaf5997c49531
Nick Chan <nk.tx.chan@gmail.com>
2023-09-13 12:04:58 -0500

    added subcommands to main command

--- src/commands/public/brawl.js
+++ src/commands/public/brawl.js
@@ -1,17 +1,29 @@
 const { SlashCommandBuilder } = require("discord.js");
 const create = require("./brawl/create");
+const enter = require("./brawl/enter");
+const start = require("./brawl/start");
 
 module.exports = {
     category: "public",
     data: new SlashCommandBuilder()
         .setName("brawl")
         .setDescription("Brawl main command.")
-        .addSubcommand(create.data),
+        .addSubcommand(create.data)
+        .addSubcommand(enter.data)
+        .addSubcommand(start.data),
     async execute(interaction) {
         const subcommand = interaction.options.getSubcommand();
         switch (subcommand) {
             case "create": {
                 await create.execute(interaction);
+                break;
+            }
+            case "enter": {
+                await enter.execute(interaction);
+                break;
+            }
+            case "start": {
+                await start.execute(interaction);
                 break;
             }
             default: {
