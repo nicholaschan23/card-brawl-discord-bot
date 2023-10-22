e610c6977fc0a537cb475d8d26861b00832efb7a
Nick Chan <nk.tx.chan@gmail.com>
2023-10-22 09:08:13 -0500

    load schedules log

--- src/functions/schedule/loadSchedules.js
+++ src/functions/schedule/loadSchedules.js
@@ -3,17 +3,16 @@ const ScheduleModel = require("../../data/schemas/scheduleSchema");
 
 async function loadSchedules() {
     try {
-        console.group("[LOAD SCHEDULES] Loading schedules...");
+        console.log("[LOAD SCHEDULES] Loading schedules...");
         const schedules = await ScheduleModel.find();
         schedules.forEach((schedule) => {
-            console.log(`Scheduling ${schedule.name}`);
+            console.log(`[LOAD SCHEDULES] Scheduling ${schedule.name}`);
             cron.schedule(schedule.cron, () => {
                 const task = require(`./tasks/${schedule.task}`);
                 task(schedule.data);
             });
         });
-        console.log("Successfully loaded schedules");
-        console.groupEnd();
+        console.log("[LOAD SCHEDULES] Successfully loaded schedules");
     } catch (error) {
         console.error("[LOAD SCHEDULES] Error loading schedules:", error);
     }
