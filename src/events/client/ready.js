1fab4cd18723cbf371d2772340e8f986d6c6c209
Nick Chan <nk.tx.chan@gmail.com>
2023-10-25 22:33:09 -0500

    load schedules import

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,6 +1,6 @@
 const { Events } = require("discord.js");
 const { loadCommands } = require("../../handlers/commandHandler");
-const loadSchedules = require("../../functions/schedule/loadSchedules");
+const { loadSchedules } = require("../../functions/schedule/loadSchedules");
 const { client } = require("../../index");
 const config = require("../../../config.json")
 
