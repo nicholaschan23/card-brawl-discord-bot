32992d5764a5ea28c1fd88776d660a9b9c0bf770
Nick Chan <nk.tx.chan@gmail.com>
2023-11-03 09:19:51 -0500

    status if statement

--- src/events/readMessages.js
+++ src/events/readMessages.js
@@ -68,7 +68,7 @@ module.exports = {
 
                 const guild = client.guilds.cache.get(config.guildID);
                 const starflight = await guild.members.fetch("816328822051045436");
-                if (starflight.presence.status === null || starflight.presence.status !== "online") { // Offline
+                if (starflight.presence.status === null) { // Offline
                     const regex = /dropping (\d+) cards/; // This regex captures the number after "dropping" and before "cards"
                     const match = message.content.match(regex);
                     if (!match) {
