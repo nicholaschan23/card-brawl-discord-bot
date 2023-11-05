948e9e179505d1dad475f09c4b236eab5e8e3452
Nick Chan <nk.tx.chan@gmail.com>
2023-11-04 20:48:56 -0500

    resume card brawl on restart

--- src/functions/schedule/tasks/startBrawl.js
+++ src/functions/schedule/tasks/startBrawl.js
@@ -11,65 +11,90 @@ const ScheduleModel = require("../../../data/schemas/scheduleSchema");
 
 async function startBrawl(data) {
     const name = data.name;
-    let setupModel;
+
+    // Get BrawlSetupModel
+    const setupModel = await BrawlSetupModel.findOne({ name }).exec();
 
     // Close card competition
-    const task = async () => {
-        setupModel = await BrawlSetupModel.findOne({ name }).exec();
-        setupModel.open = false;
-        await setupModel.save();
+    if ((setupModel.open = true)) {
+        const task = async () => {
+            setupModel.open = false;
+            await setupModel.save();
 
-        const competitorsChannel = client.channels.cache.get(config.competitorsChannelID);
-        competitorsChannel.messages.fetch(setupModel.messageID).then((message) => {
-            const updatedEmbed = getAnnouncementEmbed(setupModel);
-            updatedEmbed.setColor(config.red);
-            updatedEmbed.setFooter({
-                text: "This Card Brawl is closed!",
+            const competitorsChannel = client.channels.cache.get(config.competitorsChannelID);
+            competitorsChannel.messages.fetch(setupModel.messageID).then((message) => {
+                const updatedEmbed = getAnnouncementEmbed(setupModel);
+                updatedEmbed.setColor(config.red);
+                updatedEmbed.setFooter({
+                    text: "This Card Brawl is closed!",
+                });
+                message.edit({
+                    content: `The \`${setupModel.name}\` Card Brawl is closed! 🥊 <@&${config.competitorRole}>`,
+                    embeds: [updatedEmbed],
+                });
             });
-            message.edit({
-                content: `The \`${setupModel.name}\` Card Brawl is closed! 🥊 <@&${config.competitorRole}>`,
-                embeds: [updatedEmbed],
-            });
+        };
+        await setupModelQueue.enqueue(task);
+    }
+
+    // Get BrawlBracketModel
+    const bracketModel = await BrawlBracketModel.findOne({ name }).exec();
+    if (!bracketModel) {
+        // Create brawl bracket model in database if it doesn't exist
+        bracketModel = new BrawlBracketModel({
+            name: setupModel.name,
+            competitors: [...setupModel.cards.keys()],
         });
-    };
-    await setupModelQueue.enqueue(task);
+        await bracketModel.save();
+    }
 
-    // Create new BracketModel
-    setupModel = await BrawlSetupModel.findOne({ name }).exec();
-    const bracketModel = new BrawlBracketModel({
-        name: setupModel.name,
-        competitors: [...setupModel.cards.keys()],
-    });
-    await bracketModel.save();
-
-    // Get competitors and generate brawl bracket
+    // Get BrawlBracketHelper
     const myBrawlBracket = new BrawlBracketHelper(bracketModel, setupModel);
-    await myBrawlBracket.generateInitialBracket();
 
+    // Channel to send messages
     const judgesChannel = client.channels.cache.get(config.judgesChannelID);
-    // Introduction
-    let message = await judgesChannel.send({
-        content: `We'll be starting in \`5 minutes\`. <@&${config.judgeRole}>`,
-        embeds: [getIntroductionEmbed(setupModel)],
-    });
-    await message.react("🥳");
-    await delay(360);
-    await judgesChannel.send("## 1 minute");
-    await delay(27);
-    await judgesChannel.send("# 3");
-    await delay(1);
-    await judgesChannel.send("# 2");
-    await delay(1);
-    await judgesChannel.send("# 1");
-    await delay(1);
-    await judgesChannel.send("# Let the Card Brawl begin! 🥊");
-    await delay(2);
-    await judgesChannel.send(
-        "If you don't see your card in **Round 1**, you've received a free pass to **Round 2**!"
-    );
-    await delay(2);
 
-    // Start tournament
+    if (myBrawlBracket.getStatus() === 2) {
+        // Delete schedule
+        try {
+            const name = data.scheduleName;
+            await ScheduleModel.deleteOne({ name }).exec();
+            console.log(`[BRAWL START] ${name} schedule deleted`);
+        } catch (error) {
+            console.error(`[BRAWL START] Error deleting schedule ${name}:`, error);
+        }
+        return;
+    }
+    if (myBrawlBracket.getStatus() === 1) {
+        await judgesChannel.send(`Resuming the **${setupModel.name}** Card Brawl...`);
+    } else if (myBrawlBracket.getStatus() === 0) {
+        // Get competitors and generate brawl bracket
+        await myBrawlBracket.generateInitialBracket();
+
+        // Introduction
+        let message = await judgesChannel.send({
+            content: `We'll be starting in \`5 minutes\`. <@&${config.judgeRole}>`,
+            embeds: [getIntroductionEmbed(setupModel)],
+        });
+        await message.react("🥳");
+        await delay(360);
+        await judgesChannel.send("## 1 minute");
+        await delay(27);
+        await judgesChannel.send("# 3");
+        await delay(1);
+        await judgesChannel.send("# 2");
+        await delay(1);
+        await judgesChannel.send("# 1");
+        await delay(1);
+        await judgesChannel.send("# Let the Card Brawl begin! 🥊");
+        await delay(2);
+        await judgesChannel.send(
+            "If you don't see your card in **Round 1**, you've received a free pass to **Round 2**!"
+        );
+        await delay(2);
+    }
+
+    // Start or resume tournament
     await myBrawlBracket.conductTournament();
     await delay(3);
     message = await judgesChannel.send({
