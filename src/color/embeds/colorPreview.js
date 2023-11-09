3a0767692e46e8d6e091a625b4a5e7cf3956b36c
Nick Chan <nk.tx.chan@gmail.com>
2023-11-09 08:09:53 -0600

    added cost to preview

--- src/color/embeds/colorPreview.js
+++ src/color/embeds/colorPreview.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require("discord.js");
 const color = require("../color-config.json");
+const config = require("../../../config.json");
 
 const colors = `<@&${color.orange}>\n<@&${color.deepOrange}>\n<@&${color.red}>\n<@&${color.pink}>\n<@&${color.purple}>\n<@&${color.deepPurple}>\n<@&${color.indigo}>\n<@&${color.blue}>\n<@&${color.lightBlue}>\n<@&${color.cyan}>\n<@&${color.teal}>\n<@&${color.green}>`;
 const neonColors = `<@&${color.neonOrange}>\n<@&${color.neonDeepOrange}>\n<@&${color.neonRed}>\n<@&${color.neonPink}>\n<@&${color.neonPurple}>\n<@&${color.neonDeepPurple}>\n<@&${color.neonIndigo}>\n<@&${color.neonBlue}>\n<@&${color.neonLightBlue}>\n<@&${color.neonCyan}>\n<@&${color.neonTeal}>\n<@&${color.neonGreen}>`;
@@ -8,7 +9,7 @@ function getPreviewEmbed() {
     const embed = new EmbedBuilder()
         .setTitle("Color Role")
         .setDescription("Select a color using the dropdown menu below.")
-        .addFields({ name: `Regular`, value: colors, inline: true }, { name: "Neon", value: neonColors, inline: true });
+        .addFields({ name: `${color.cost} ${config.emojiToken}`, value: colors, inline: true }, { name: `${color.neonCost} ${config.emojiToken}`, value: neonColors, inline: true });
     return embed;
 }
 
