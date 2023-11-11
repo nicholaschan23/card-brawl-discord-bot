52a491af9f3eecc54daefb981a0db9de32c016cd
Nick Chan <nk.tx.chan@gmail.com>
2023-11-10 23:39:39 -0600

    token help

--- src/commands/public/help/token.js
+++ src/commands/public/help/token.js
@@ -10,12 +10,16 @@ module.exports = {
     async execute(interaction) {
         const QA = [
             {
-                question: `What is a ${token}?`,
-                answer: `${token} are Tokens, a currency to interact with various exclusive features in the Far Shore community.`,
+                question: `What are tokens?`,
+                answer: `Tokens are Far Shore's exclusive form of currency. Spend them to elevate your presence in the community, unlocking visual flair, event bonuses, and more!`,
             },
             {
-                question: `How do you earn ${token}?`,
-                answer: `Every 5 drops rewards you a ${token}! Drop cards with any of these bots:\n- <@${config.karutaID}>\n- <@${config.sofiID}>\n- <@${config.tofuID}>\n- <@${config.gachaponID}>\nDrops will only count every 30 minutes. See <#1144103186974646402> for more details on how to drop.`,
+                question: `How do you earn tokens?`,
+                answer: `Tokens are earned through community engagement.\n 1. Every 5 drops rewards you a ${token}! Drops count every 30 minutes and can be from any of the bots below. See <#1144103186974646402> for details.\n - <@${config.karutaID}>\n - <@${config.sofiID}>\n - <@${config.tofuID}>\n - <@${config.gachaponID}>`,
+            },
+            {
+                question: `What can I spend tokens on?`,
+                answer: `Currently, the available options to spend tokens on are listed below. More features coming soon!\n- Role color (Use command \`/role color\`)`,
             },
         ];
 
