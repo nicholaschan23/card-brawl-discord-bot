69dd5029f53494ebf07b27204a90646218a05ab9
Nick Chan <nk.tx.chan@gmail.com>
2023-11-14 10:07:25 -0600

    view participants done

--- src/giveaway/src/viewParticipants.js
+++ src/giveaway/src/viewParticipants.js
@@ -1,15 +1,48 @@
-const client = require("../../index");
+const { EmbedBuilder } = require("discord.js");
+const config = require("../../../config.json");
+const buttonPages = require("../../support/src/pagination")
 const GiveawayModel = require("../schemas/giveawaySchema");
-const UserInventoryModel = require("../../inventory/schemas/userInventorySchema");
 
 async function viewParticipants(interaction) {
     const messageID = interaction.message.id;
-    const userID = interaction.user.id;
 
     const giveawayModel = await GiveawayModel.findOne({ messageID }).exec();
-    const inventoryModel = await UserInventoryModel.findOne({ userID }).exec();
+    if (!giveawayModel) {
+        console.error("Couldn't retrieve Giveaway Model");
+        return;
+    }
 
-    await interaction.reply({ content: "", ephemeral: true });
+    // Format competitors into lines
+    let lines = [];
+    const mapArray = Array.from(giveawayModel.entries.entries());
+    mapArray.forEach(([key, value]) => {
+        const entries =
+            value == 1
+                ? `(**1** ${config.emojiToken} entry)`
+                : `(**${value}** ${config.emojiToken} entries)`;
+        lines.push(`${lines.length + 1}. <@${key}> ${entries}`);
+    });
+
+    // Create page embeds
+    let pages = [];
+    let description = "";
+    for (let i = 0, lineNum = 0, pageNum = 1; i < lines.length; i++) {
+        description += lines[i] + "\n";
+        lineNum++;
+
+        if (lineNum === 10 || i === lines.length - 1) {
+            const embed = new EmbedBuilder()
+                .setTitle(`Giveaway`)
+                .setDescription("**Participants**:\n" + description)
+                .setFooter({ text: `Page ${pageNum}` });
+            pages.push(embed);
+
+            lineNum = 0;
+            pageNum++;
+            description = "";
+        }
+    }
+    buttonPages(interaction, pages, true);
 }
 
 module.exports = viewParticipants;
