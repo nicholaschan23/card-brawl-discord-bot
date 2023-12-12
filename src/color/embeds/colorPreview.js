645f5bc7dce855c759f12670c29f598bc419d093
Nick Chan <nick.chan@utexas.edu>
2023-12-12 13:42:16 -0600

    color embed

--- src/color/embeds/colorPreview.js
+++ src/color/embeds/colorPreview.js
@@ -2,27 +2,27 @@ const { EmbedBuilder } = require("discord.js");
 const config = require("../../../config.json");
 
 const colors =
-    `${config.roleID.red}\n` +
-    `${config.roleID.pink}\n` +
-    `${config.roleID.purple}\n` +
-    `${config.roleID.deepPurple}\n` +
-    `${config.roleID.indigo}\n` +
-    `${config.roleID.blue}\n` +
-    `${config.roleID.lightBlue}\n` +
-    `${config.roleID.cyan}\n` +
-    `${config.roleID.teal}\n` +
-    `${config.roleID.green}`;
+    `<@&${config.roleID.red}>\n` +
+    `<@&${config.roleID.pink}>\n` +
+    `<@&${config.roleID.purple}>\n` +
+    `<@&${config.roleID.deepPurple}>\n` +
+    `<@&${config.roleID.indigo}>\n` +
+    `<@&${config.roleID.blue}>\n` +
+    `<@&${config.roleID.lightBlue}>\n` +
+    `<@&${config.roleID.cyan}>\n` +
+    `<@&${config.roleID.teal}>\n` +
+    `<@&${config.roleID.green}>`;
 const neonColors =
-    `${config.roleID.neonRed}\n` +
-    `${config.roleID.neonPink}\n` +
-    `${config.roleID.neonPurple}\n` +
-    `${config.roleID.neonDeepPurple}\n` +
-    `${config.roleID.neonIndigo}\n` +
-    `${config.roleID.neonBlue}\n` +
-    `${config.roleID.neonLightBlue}\n` +
-    `${config.roleID.neonCyan}\n` +
-    `${config.roleID.neonTeal}\n` +
-    `${config.roleID.neonGreen}`;
+    `<@&${config.roleID.neonRed}>\n` +
+    `<@&${config.roleID.neonPink}>\n` +
+    `<@&${config.roleID.neonPurple}>\n` +
+    `<@&${config.roleID.neonDeepPurple}>\n` +
+    `<@&${config.roleID.neonIndigo}>\n` +
+    `<@&${config.roleID.neonBlue}>\n` +
+    `<@&${config.roleID.neonLightBlue}>\n` +
+    `<@&${config.roleID.neonCyan}>\n` +
+    `<@&${config.roleID.neonTeal}>\n` +
+    `<@&${config.roleID.neonGreen}>`;
 
 function getPreviewEmbed() {
     const embed = new EmbedBuilder()
